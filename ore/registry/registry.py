d6a30da4223e3544841e4ccab6e9f5ab49646a25
Evan Leis <evan.explodes@gmail.com>
2017-11-09 20:06:42 -0800

    [registry] registry loads dynamically, on demand

--- ore/registry/registry.py
+++ ore/registry/registry.py
@@ -1,7 +1,9 @@
+import glob
+from os.path import dirname, basename, isfile
+
 from ore.core.effects import Effect
 
-
-REGISTRY = {}
+_REGISTRY = {}
 
 
 def register_effect(effect: Effect):
@@ -11,6 +13,19 @@ def register_effect(effect: Effect):
     An effect be of type openrazer_effects.core.effects.Effect to be valid.
     """
     cli_name = effect.get_cli_name(None)
-    if cli_name in REGISTRY:
+    if cli_name in _REGISTRY:
         raise ValueError("effect {} already registered.".format(cli_name))
-    REGISTRY[cli_name] = effect
+    _REGISTRY[cli_name] = effect
+
+
+def get_registry():
+    _load_all_submodules()
+    return _REGISTRY
+
+
+def _load_all_submodules():
+    import ore.effects
+    modules = glob.glob(dirname(ore.effects.__file__) + "/*.py")
+    files = ["ore.effects.{}".format(basename(f)[:-3]) for f in modules if isfile(f) and not f.endswith('__init__.py')]
+    f = list(map(__import__, files))
+    return f
--- test/registry_test.py
+++ test/registry_test.py
@@ -1,3 +1,42 @@
+import pytest
+import six
+
+from ore.registry.registry import get_registry
+
+
 def test_registry_not_empty():
-    from ore.registry.registry import REGISTRY
-    assert len(REGISTRY) > 0
+    reg = get_registry()
+    assert len(reg) > 0
+
+
+def is_str(s):
+    return type(s) in six.string_types
+
+
+@pytest.mark.parametrize("cli_name,effect", get_registry().items())
+def test_effect_conforms_to_Effect(cli_name, effect):
+    # cli_name
+    assert is_str(cli_name)
+
+    c = effect.get_cli_name(None)
+    assert c == cli_name
+
+    assert is_str(cli_name)
+
+    # name
+    assert is_str(effect.get_name(None))
+
+    # author
+    assert is_str(effect.get_author(None))
+
+    # type
+    assert effect.get_type(None) in ["keyboard", "mouse"]
+
+    # description
+    description = effect.get_description(None)
+    if description is not None:
+        assert is_str(description)
+
+    # start
+    assert hasattr(effect, "start")
+    assert callable(effect.start)
